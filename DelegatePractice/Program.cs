4d4e34f897bbe9db8b37acc9a661f4eb43f8c254
moleNULL <94998782+moleNULL@users.noreply.github.com>
2022-12-04 11:26:26 +0200

    Add StyleCop with settings.ruleset

--- DelegatePractice/Program.cs
+++ DelegatePractice/Program.cs
@@ -2,9 +2,9 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine(" ");
         }
     }
 }
