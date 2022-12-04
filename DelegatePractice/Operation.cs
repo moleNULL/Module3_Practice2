e31e8907dbf3c3b5e1f66b9a0a348f383f78246e
moleNULL <94998782+moleNULL@users.noreply.github.com>
2022-12-04 15:21:04 +0200

    Implemented delegate practice task

---
+++ DelegatePractice/Operation.cs
@@ -0,0 +1,32 @@
+﻿namespace DelegatePractice
+{
+    internal class Operation
+    {
+        public event OperationHandler? SumEvent;
+
+        // Implements logic of counting results of methods passed to SumEvent
+        public int GetOperationResultSum(int x, int y)
+        {
+            int sum = 0;
+
+            var ops = SumEvent?.GetInvocationList();
+
+            if (ops is null)
+            {
+                throw new Exception("SumEvent is null");
+            }
+
+            foreach (var op in ops)
+            {
+                object? o = op.DynamicInvoke(x, y);
+
+                if (o is int i)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ DelegatePractice/Starter.cs
@@ -0,0 +1,63 @@
+﻿namespace DelegatePractice
+{
+    public delegate int OperationHandler(int x, int y);
+    internal class Starter
+    {
+        public static void Run()
+        {
+            int num1 = 24;
+            int num2 = 2;
+
+            Console.WriteLine($"Num1: {num1}\nNum2: {num2}\n");
+            ConsoleColored("Subsribing to SumEvent with Sum() twice\n\ninvoking GetSumResults()\n", ConsoleColor.Green);
+
+            var operation = new Operation();
+            operation.SumEvent += Sum;
+            operation.SumEvent += Sum;
+
+            int? res = GetSumResults(operation, num1, num2);
+
+            if (res is not null)
+            {
+                Console.WriteLine($"Result: ({num1} + {num2}) * 2 = {res.Value}\n");
+            }
+
+            operation.SumEvent -= Sum;
+            operation.SumEvent -= Sum;
+
+            ConsoleColored("Unsubsribing from SumEvent with Sum() twice\n\ninvoking GetSumResults()\n", ConsoleColor.Red);
+
+            res = GetSumResults(operation, num1, num2);
+
+            if (res is not null)
+            {
+                Console.WriteLine($"Result: ({num1} + {num2}) * 2 = {res.Value}");
+            }
+        }
+
+        private static int Sum(int x, int y) => x + y;
+
+        private static int? GetSumResults(Operation operation, int x, int y)
+        {
+            int? res = null;
+
+            try
+            {
+                res = operation.GetOperationResultSum(x, y);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception! {ex.Message}");
+            }
+
+            return res;
+        }
+
+        private static void ConsoleColored(string data, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(data);
+            Console.ResetColor();
+        }
+    }
+}
