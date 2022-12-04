0344e8484f73e4d3e55b194e327c8df3a3df3fdf
moleNULL <94998782+moleNULL@users.noreply.github.com>
2022-12-04 15:21:04 +0200

    Implemented delegate practice task

--- DelegatePractice/Program.cs
+++ DelegatePractice/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace DelegatePractice
+﻿/*
+                                                      Задача
+На тему делегатів:
+-Написати метод, який рахує суму двох чисел.
+-Двічі методом для підрахунку сум підписатися на подію.
+-Порахувати суму результатів обчислень методів.
+-Логіка сум результатів повинна бути обернена в метод обгортки, який містить у  собі try-catch.
+-Конструкція try-catch має бути винесена в окремий метод. Усередині конструкції try виконується
+    логіка якогось методу і цей метод передається як параметр.
+
+ */
+
+namespace DelegatePractice
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(" ");
+            Starter.Run();
+
+            Console.Write("\nPress any key to continue . . .");
+            Console.ReadKey();
         }
     }
 }
